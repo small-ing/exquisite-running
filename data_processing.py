c8c97df1050ce6baf8c72a33fa3b01755befb585
Travis Smalling <main@smalling.dev>
2023-07-27 13:56:33 -0500

    update to data_processing

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,6 @@
+import landmark
+
+
 # read in images, pass them through the landmark, and save tensors that store the landmark data
 
 # take the generated tensors, and pass them through a CNN to generate a prediction
