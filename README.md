a1355bcd9c238f9c45862238620e6eb9bf0618fa
Travis Smalling <main@smalling.dev>
2023-07-27 08:31:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# exquisite-running
+ 
---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2
+
+# Path: main.py
+camera = cv2.VideoCapture(0)
+
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+
+
+while True:
+    try:
+        success, frame = camera.read()
+    except:
+        print("There is no camera")
+        break
+    
+    #insert any model calls here
+
+    
+    cv2.imshow("Camera", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
