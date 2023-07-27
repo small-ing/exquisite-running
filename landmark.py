816d945ce669e946727a898d9924b0dee055ff3e
Travis Smalling <main@smalling.dev>
2023-07-27 12:39:30 -0500

    math

--- landmark.py
+++ landmark.py
@@ -1,9 +1,20 @@
 # imports
-import mediapipe as mp
+
+# need python 3.7.9 for mediapipe
+import mediapipe as mp # pip install mediapipe==0.10.2
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import numpy as np
 import cv2
+
+
+def calculate_angle(start, shared, end):
+    vec_1, vec_2 = shared - start, end - shared # (x, y) vectors
+    vec_1_magnitude, vec_2_magnitude = np.linalg.norm(vec_1), np.linalg.norm(vec_2)
+    dot_prd = np.dot(vec_1, vec_2)
+    angle = np.arccos(dot_prd / (vec_1_magnitude * vec_2_magnitude))
+    return angle
+    
 
 # Path: landmark.py
 model_path = 'pose_landmarker.task'
