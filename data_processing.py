8698db1134ca4c2bcb7f6b17f2644dbf8ef32865
Travis Smalling <main@smalling.dev>
2023-07-28 10:03:00 -0500

    adjusted landmark to OOP standards

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,49 @@
-import landmark
+from landmark import Tracker
+import torch
+import torch.nn as nn
+import numpy as np
+import os
+import mediapipe as mp
 
-
+landmarker = Tracker()
 # read in images, pass them through the landmark, and save tensors that store the landmark data
+def collect_data(batch_size):
+    pass
+    folder_path = 'data'
+    '''
+    Each landmark has 4 values:
+        X, Y both normalized with respect to the image size
+        
+    X coordinate
+    Y coordinate
+    Z coordinate (Depth with respect to hips)
+    Visibility between 0-1
+    '''
+    
+    empty_landmarks = np.zeros((batch_size, 33, 4)) # 33 landmarks, 4 values per landmark
+    empty_labels = np.zeros((batch_size)) # 1 label per image
+    
+    # Check if the folder exists
+    if not os.path.exists(folder_path):
+        print(f"Error: Folder '{folder_path}' does not exist.")
+        return
+
+    # Get a list of image files in the folder
+    image_files = [file for file in os.listdir(folder_path) if file.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp"))]
+
+    # Process each image in the folder
+    for image_file in image_files:
+        image_path = os.path.join(folder_path, image_file)
+        mp_image = mp.Image.create_from_file(image_path)
+        
+        landmarker.final_landmarker.detect(mp_image).pose_landmarks[0] # this is the landmark data we need to save in the tensors
+    
+    
+    
+
 
 # take the generated tensors, and pass them through a CNN to generate a prediction
+
+
+if __name__ == "__main__":
+    
--- landmark.py
+++ landmark.py
@@ -9,56 +9,63 @@ from mediapipe.framework.formats import landmark_pb2
 import numpy as np
 import cv2
 
-
-def calculate_angle(start, shared, end):
-    vec_1, vec_2 = shared - start, end - shared # (x, y) vectors
-    vec_1_magnitude, vec_2_magnitude = np.linalg.norm(vec_1), np.linalg.norm(vec_2)
-    dot_prd = np.dot(vec_1, vec_2)
-    angle = np.arccos(dot_prd / (vec_1_magnitude * vec_2_magnitude))
-    return angle
-    
-def draw_landmarks_on_image(rgb_image, detection_result):
-  pose_landmarks_list = detection_result.pose_landmarks
-  annotated_image = np.copy(rgb_image)
-
-  # Loop through the detected poses to visualize.
-  for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
-
-    # Draw the pose landmarks.
-    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    pose_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
-  return annotated_image
-
-# Path: landmark.py
-model_path = 'pose_landmarker_lite.task'
-
-BaseOptions = mp.tasks.BaseOptions
-PoseLandmarker = mp.tasks.vision.PoseLandmarker
-PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
-
-options = PoseLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.IMAGE)
-
-with PoseLandmarker.create_from_options(options) as landmarker:
-        # Load the input image from an image file.
-    mp_image = mp.Image.create_from_file('bad running form169.jpg')
-    pose_landmarker_result = landmarker.detect(mp_image)
-    #save pose_landmarker_result to a file
-    #print(pose_landmarker_result)
-    annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), pose_landmarker_result)
-    while True:
-        cv2.imshow("Image", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+class Tracker():
+    def __init__(self):
+        self.model_path = 'pose_landmarker_lite.task'
+        self.base_options = mp.tasks.BaseOptions
+        self.pose_landmarker = mp.tasks.vision.PoseLandmarker
+        self.landmarker_options = mp.tasks.vision.PoseLandmarkerOptions
+        self.running_mode = mp.tasks.vision.RunningMode
+        self.options = self.landmarker_options(
+            base_options=self.base_options(model_asset_path=self.model_path),
+            running_mode=self.running_mode.IMAGE)
         
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cv2.destroyAllWindows()
+        self.final_landmarker = self.pose_landmarker.create_from_options(self.options)
+                
+        
+    def calculate_angle(start, shared, end):
+        vec_1, vec_2 = shared - start, end - shared # (x, y) vectors
+        vec_1_magnitude, vec_2_magnitude = np.linalg.norm(vec_1), np.linalg.norm(vec_2)
+        dot_prd = np.dot(vec_1, vec_2)
+        angle = np.arccos(dot_prd / (vec_1_magnitude * vec_2_magnitude))
+        return angle
+        
+    def draw_landmarks_on_image(self, rgb_image, detection_result):
+        pose_landmarks_list = detection_result.pose_landmarks
+        annotated_image = np.copy(rgb_image)
+
+        # Loop through the detected poses to visualize.
+        for idx in range(len(pose_landmarks_list)):
+            pose_landmarks = pose_landmarks_list[idx]
+
+            # Draw the pose landmarks.
+            pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            pose_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+            ])
+            solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            pose_landmarks_proto,
+            solutions.pose.POSE_CONNECTIONS,
+            solutions.drawing_styles.get_default_pose_landmarks_style())
+        return annotated_image
+
+    def detect_and_draw(self, image):
+        marks = self.final_landmarker.detect(image)
+        return self.draw_landmarks_on_image(image.numpy_view(), marks)
+    
+# Path: landmark.py
+
+if __name__ == "__main__":
+    pTracker = Tracker()
+    with pTracker.final_landmarker as landmarker:
+            # Load the input image from an image file.
+        mp_image = mp.Image.create_from_file('data/bad running form169.jpg')
+        # print(pTracker.final_landmarker.detect(mp_image).pose_landmarks[0])
+        annotated_image = pTracker.detect_and_draw(mp_image)
+        while True:
+            cv2.imshow("Image", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cv2.destroyAllWindows()
